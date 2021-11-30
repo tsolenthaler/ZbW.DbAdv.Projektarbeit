f1f45d52f3503d9412e29c579ae1d1e2bc5fd894
corinawidmer <corinawidmer@gmx.ch>
2021-11-30 14:15:13 +0100

    Kommentar hinzugefügt

--- ZbW.DbAdv.Projektarbeit/AssemblyInfo.cs
+++ ZbW.DbAdv.Projektarbeit/AssemblyInfo.cs
@@ -8,3 +8,6 @@ using System.Windows;
                                               //(used if a resource is not found in the page,
                                               // app, or any theme specific resource dictionaries)
 )]
+
+
+// Hallo ich bins Corina
