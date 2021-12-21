d77217364d18976a8e9af30501a5e09f2f133e67
Angelo Lütolf <angelo.luetolf93@gmail.com>
2021-12-21 18:23:23 +0100

    added lists to businessManager

--- BusinessLayer/BusinessManager.cs
+++ BusinessLayer/BusinessManager.cs
@@ -8,5 +8,9 @@ namespace BusinessLayer
 {
     internal class BusinessManager
     {
+        private List<Customer> customerList;
+        private List<Article> articleList;
+        private List<ArticelGroup> articelGroupList;
+        private List<Order> orderList;
     }
 }
