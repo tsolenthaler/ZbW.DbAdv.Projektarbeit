f996278dbdb25f501c6168816698695eab37709c
Thomas Solenthaler <tsolenthaler@gmail.com>
2022-02-13 12:52:57 +0100

    fix dataccessmanager check datastore and SeedDB

--- DataAccessLayer/Context/SeedDB.cs
+++ DataAccessLayer/Context/SeedDB.cs
@@ -74,33 +74,43 @@ namespace DataAccessLayer.Context
 
         public List<ArticleDTO> GenerateArticleDTOs()
         {
+            using var context = new SetupDB();
+            var articleGroup = context.ArticelGroups.Where(c => c.Name == "T-Shirt").First();
+
             var article = new List<ArticleDTO>()
             {
-                new ArticleDTO() { Name = "Sonnen T-Shirt", Price = (decimal)14.50, ArticleGroupId = 2 },
-                new ArticleDTO() { Name = "Mond T-Shirt", Price = (decimal)19.50, ArticleGroupId = 2 },
-                new ArticleDTO() { Name = "Sterne T-Shirt", Price = (decimal)9.50, ArticleGroupId = 2 }
+                new ArticleDTO() { Name = "Sonnen T-Shirt", Price = (decimal)14.50, ArticleGroupId = articleGroup.Id },
+                new ArticleDTO() { Name = "Mond T-Shirt", Price = (decimal)19.50, ArticleGroupId = articleGroup.Id },
+                new ArticleDTO() { Name = "Sterne T-Shirt", Price = (decimal)9.50, ArticleGroupId = articleGroup.Id }
             };
             return article;
         }
 
         public List<OrderPositionDTO> GenerateOrderPositionDTOs()
         {
+            using var context = new SetupDB();
+            var articleFirst = context.Articles.First();
+            //var articleLast = context.Articles.Last();
+            var order = context.Orders.First();
             var orderposition = new List<OrderPositionDTO>
             {
-                new OrderPositionDTO() { Quantity = 5, ArticleId = 1, OrderId = 1},
-                new OrderPositionDTO() { Quantity = 2, ArticleId = 2, OrderId = 1 },
-                new OrderPositionDTO() { Quantity = 11, ArticleId = 3,  OrderId = 1 }
+                new OrderPositionDTO() { Quantity = 5, ArticleId = articleFirst.Id, OrderId = order.Id},
+                new OrderPositionDTO() { Quantity = 2, ArticleId = articleFirst.Id, OrderId = order.Id },
+                new OrderPositionDTO() { Quantity = 11, ArticleId = articleFirst.Id,  OrderId = order.Id }
             };
             return orderposition;
         }
 
         public List<OrderDTO> GenerateOrderDTOs()
         {
+            using var context = new SetupDB();
+            var customerFirst = context.Customers.First();
+            //var customerLast = context.Customers.Last();
             var order = new List<OrderDTO>
             {
-                new OrderDTO { Date = new DateTime(2021, 3, 15), CustomerId = 1 },
-                new OrderDTO { Date = new DateTime(2022, 1, 15), CustomerId = 2},
-                new OrderDTO { Date = new DateTime(2022, 1, 30), CustomerId = 3}
+                new OrderDTO { Date = new DateTime(2021, 3, 15), CustomerId = customerFirst.Id },
+                new OrderDTO { Date = new DateTime(2022, 1, 15), CustomerId = customerFirst.Id},
+                new OrderDTO { Date = new DateTime(2022, 1, 30), CustomerId = customerFirst.Id}
             };
             return order;
         }
