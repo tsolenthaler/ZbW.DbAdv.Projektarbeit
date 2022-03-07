bb149552d4a710d2e0b2471d10b6432f70bcb746
Thomas Solenthaler <tsolenthaler@gmail.com>
2022-03-07 20:17:59 +0100

    fix datetime format for sql

--- DataAccessLayer/DataAccessManager.cs
+++ DataAccessLayer/DataAccessManager.cs
@@ -498,12 +498,16 @@ namespace DataAccessLayer.Models
         {
             var invoiceReports = new List<InvoiceReportDTO>();
 
-            String startDate2 = new DateTime(startDate.Year, startDate.Month, startDate.Day).ToString();
-            String endDate2 = new DateTime(endDate.Year, endDate.Month, endDate.Day).ToString();
+            DateTime startDate2 = new DateTime(startDate.Year, startDate.Month, startDate.Day);
+            DateTime endDate2 = new DateTime(endDate.Year, endDate.Month, endDate.Day);
+
+            string startDateFormat = startDate.ToString("s");
+            string endDateFormat = endDate.ToString("s");
 
             using var context = new SetupDB();
             using var command = context.Database.GetDbConnection().CreateCommand();
-            command.CommandText = $"SELECT [CustomerId], c.Company as Name, a.Street as Street, a.StreetNo as StreetNo, a.Plz as Plz, a.City as City, a.Countryname as Countryname,i.[Id],i.[Date] As Date,[Netto],[Brutto], i.ValidFrom as ValidFrom, i.ValidTo as ValidTo FROM [AuftragsverwaltungHistory].[dbo].[Invoices] i INNER JOIN dbo.Customer FOR SYSTEM_TIME ALL as c on i.customerid = c.id INNER JOIN dbo.Addresses FOR SYSTEM_TIME ALL as a on c.addressid = a.id WHERE i.Date BETWEEN '{startDate2}' and '{endDate2}' AND c.ValidFrom <= i.Date and c.ValidTo >= i.Date AND a.ValidFrom <= i.Date and a.ValidTo >= i.Date ORDER BY i.Date";
+            command.CommandText = $"SELECT [CustomerId], c.Company as Name, a.Street as Street, a.StreetNo as StreetNo, a.Plz as Plz, a.City as City, a.Countryname as Countryname,i.[Id],i.[Date] As Date,[Netto],[Brutto], i.ValidFrom as ValidFrom, i.ValidTo as ValidTo FROM [AuftragsverwaltungHistory].[dbo].[Invoices] i INNER JOIN dbo.Customer FOR SYSTEM_TIME ALL as c on i.customerid = c.id INNER JOIN dbo.Addresses FOR SYSTEM_TIME ALL as a on c.addressid = a.id WHERE i.Date BETWEEN '{startDateFormat}' and '{endDateFormat}' AND c.ValidFrom <= i.Date and c.ValidTo >= i.Date AND a.ValidFrom <= i.Date and a.ValidTo >= i.Date ORDER BY i.Date";
+
             context.Database.OpenConnection();
             using var reader = command.ExecuteReader();
             while (reader.Read())
