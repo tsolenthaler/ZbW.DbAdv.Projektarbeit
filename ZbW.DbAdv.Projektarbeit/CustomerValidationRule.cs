6b61d439e67737c21f1231c193e2afc15efacc87
Thomas Solenthaler <tsolenthaler@gmail.com>
2022-08-28 14:26:49 +0200

    test validation check clientnr with regex

---
+++ ZbW.DbAdv.Projektarbeit/CustomerValidationRule.cs
@@ -0,0 +1,32 @@
+﻿using BusinessLayer.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Input;
+
+namespace PresentationLayer
+{
+    public class CustomerValidationRule : ValidationRule
+    {
+        public override ValidationResult Validate(object value,
+            System.Globalization.CultureInfo cultureInfo)
+        {
+            Customer customer = (value as BindingGroup).Items[0] as Customer;
+            string pattern = @"^CU\d{5}$";
+            if (!Regex.IsMatch(customer.Clientnr, pattern))
+            {
+                return new ValidationResult(false,
+                    "Clientnr muss mit 'CU' anfangen und 5 Zahlen enthalten");
+            }
+            else
+            {
+                return ValidationResult.ValidResult;
+            }
+        }
+    }
+}
